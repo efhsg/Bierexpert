543de9d3efffb052a7ae517cf989f40e589ec8c8
Erwin Sollewijn Gelpke <efhsg@live.nl>
2017-04-30 17:25:03 +0200

    mvp 0.1.1

---
+++ app/src/test/java/nl/differentcook/bierexpert/presenter/VindBierPresenterTest.java
@@ -0,0 +1,21 @@
+package nl.differentcook.bierexpert.presenter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by esg on 30/04/2017.
+ */
+public class VindBierPresenterTest {
+    @Test
+    public void laadLanden() throws Exception {
+
+    }
+
+    @Test
+    public void laadMerken() throws Exception {
+
+    }
+
+}
